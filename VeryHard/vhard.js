76922602709353e02e0fdec4de83aeb748e30df4
elenabaskin <ellebaskin@gmail.com>
2022-09-15 17:35:09 -0400

    added while statements to very hard challenge

--- VeryHard/vhard.js
+++ VeryHard/vhard.js
@@ -1,37 +1,47 @@
-let x = parseFloat(prompt("♡ଘ(੭ˊᵕˋ)੭* ੈ✩‧˚: Pick a number!", "0"));
+let x = Number(prompt("♡ଘ(੭ˊᵕˋ)੭* ੈ✩‧˚: Pick a number!", "0"));
+
 console.log(x);
+
+while (isNaN(x)) {
+  x = prompt("(｡•́︿•̀｡): Please pick a number!", "0");
+}
 
 let operator = prompt("♡ଘ(੭ˊᵕˋ)੭* ੈ✩‧˚: Pick an operator: +, -, *, /");
 
-let y = parseFloat(prompt("♡ଘ(੭ˊᵕˋ)੭* ੈ✩‧˚: Pick a second number!", "0"));
+let y = Number(prompt("♡ଘ(੭ˊᵕˋ)੭* ੈ✩‧˚: Pick a second number!", "0"));
+
 console.log(y);
+
+while (isNaN(y)) {
+  y = prompt("(⋟﹏⋞): Please pick another number!", "0");
+}
 
 switch (operator) {
   case "+":
     result = x + y;
     console.log(`${x} + ${y} = ${result}`);
-    alert(`ଘ(੭ˊᵕˋ)੭: [${x} + ${y} = ${result}]  ♡* ੈ✩‧`);
+    alert(`✧･ﾟ: *✧･ﾟ:* [${x} + ${y} = ${result}]  *:･ﾟ✧*:･ﾟ✧`);
     break;
 
   case "-":
     result = x - y;
     console.log(`${x} - ${y} = ${result}`);
-    alert(`ଘ(੭ˊᵕˋ)੭: [${x} - ${y} = ${result}]  ♡* ੈ✩‧`);
+    alert(`✧･ﾟ: *✧･ﾟ:* [${x} - ${y} = ${result}]  *:･ﾟ✧*:･ﾟ✧`);
     break;
 
   case "*":
     result = x * y;
     console.log(`${x} * ${y} = ${result}`);
-    alert(`ଘ(੭ˊᵕˋ)੭: [${x} * ${y} = ${result}]  ♡* ੈ✩‧`);
+    alert(`✧･ﾟ: *✧･ﾟ:* [${x} * ${y} = ${result}]  *:･ﾟ✧*:･ﾟ✧`);
     break;
 
   case "/":
     result = x / y;
     console.log(`${x} / ${y} = ${result}`);
-    alert(`ଘ(੭ˊᵕˋ)੭: [${x} / ${y} = ${result}]  ♡* ੈ✩‧`);
+    alert(`✧･ﾟ: *✧･ﾟ:* [${x} / ${y} = ${result}]  *:･ﾟ✧*:･ﾟ✧`);
     break;
 
   default:
     console.log("The user isn't following directions (｡•́︿•̀｡)");
-    alert("Please follow the directions ✿ ʕ •ᴥ•ʔ");
+    alert("ʕ •ᴥ•ʔ✿: Please follow the directions!");
 }
